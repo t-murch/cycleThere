133d73bc340b06e83aa2e49c5a388e0474728b61
Todd Murchison Jr <t.murchjr@gmail.com>
2019-09-16 21:35:34 -0500

    boilerplate complete

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
+
+const App = () => {
+  return <div>Cycle There!</div>;
+};
+
+export default App;
