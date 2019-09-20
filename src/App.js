4ec8872efc30b7b02c056fc12a162f6c7aee5b08
Todd Murchison Jr <t.murchjr@gmail.com>
2019-09-20 15:05:59 -0500

    Initial Map Rendering Completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
+import MapComp from "./MapComp";
 
 const App = () => {
-  return <div>Cycle There!</div>;
+  return (
+    <>
+      <div>Cycle There!</div>
+      <div>
+        <MapComp></MapComp>
+      </div>
+    </>
+  );
 };
 
 render(<App />, document.getElementById("root"));
