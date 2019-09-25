a9f8f7e0b517ca4160cc1b3715abede19a2d8e49
Todd Murchison Jr <t.murchjr@gmail.com>
2019-09-25 15:53:55 -0500

    Landing Page partially configured, displaying Header, Main, and Sidebar. Installed Grommet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
+import { Grommet, Box, Heading, Button } from "grommet";
+import { Menu } from "grommet-icons";
+import customTheme from "./theme";
+import MainContainer from "./MainContainer";
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
 
 const App = () => {
-  return <div>Cycle There!</div>;
+  return (
+    <>
+      <Grommet theme={customTheme} full>
+        <Box fill>
+          <AppBar>
+            <Heading level="3" margin="none">
+              Cycle There!
+            </Heading>
+            <Button icon={<Menu />} onClick={() => {}} />
+          </AppBar>
+          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+            <Box flex align="center" justify="center">
+              Body for the Map.
+            </Box>
+            <Box
+              width="medium"
+              background="light-2"
+              elevation="small"
+              align="center"
+              justify="center"
+            >
+              SideBar for Settings.
+            </Box>
+          </Box>
+        </Box>
+      </Grommet>
+    </>
+  );
 };
 
 render(<App />, document.getElementById("root"));
---
+++ src/MainContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { css } from "@emotion/core";
+
+const MainContainer = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default MainContainer;
