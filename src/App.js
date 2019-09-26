d0196d54c772b70a1a7d46eeccdecdba335bcebb
Todd Murchison Jr <t.murchjr@gmail.com>
2019-09-25 19:10:41 -0500

    1.001 UI Landing Page w/Settings Tab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
-import { render } from "react-dom";
-import { Router } from "@reach/router";
-import { Box, Button, Grommet, Heading, Collapsible } from "grommet";
-import { Menu } from "grommet-icons";
-import customTheme from "./theme";
-import MainContainer from "./MainContainer";
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { Router } from '@reach/router';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Grommet,
+  Heading,
+  Layer,
+  ResponsiveContext
+} from 'grommet';
+import { Menu, FormClose } from 'grommet-icons';
+import customTheme from './theme';
+import MainContainer from './MainContainer';
 
 const AppBar = props => (
   <Box
@@ -13,9 +21,9 @@ const AppBar = props => (
     align="center"
     justify="between"
     background="brand"
-    pad={{ left: "medium", right: "small", vertical: "small" }}
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
     elevation="medium"
-    style={{ zIndex: "1" }}
+    style={{ zIndex: '1' }}
     {...props}
   />
 );
@@ -26,37 +34,66 @@ const App = () => {
   return (
     <>
       <Grommet theme={customTheme} full>
-        <Box fill>
-          <AppBar>
-            <Heading level="3" margin="none">
-              Cycle There!
-            </Heading>
-            <Button
-              icon={<Menu />}
-              onClick={() => setShowSidebar(!showSidebar)}
-            />
-          </AppBar>
-          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
-            <Box flex align="center" justify="center">
-              Body for the Map.
-            </Box>
-            <Collapsible direction="horizontal" open={showSidebar}>
-              <Box
-                flex
-                width="medium"
-                background="light-2"
-                elevation="small"
-                align="center"
-                justify="center"
-              >
-                SideBar for Settings.
+        <ResponsiveContext.Consumer>
+          {size => (
+            <Box fill>
+              <AppBar>
+                <Heading level="3" margin="none">
+                  Cycle There!
+                </Heading>
+                <Button
+                  icon={<Menu />}
+                  onClick={() => setShowSidebar(!showSidebar)}
+                />
+              </AppBar>
+              <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                <Box flex align="center" justify="center">
+                  Body for the Map.
+                </Box>
+                {!showSidebar || size !== 'small' ? (
+                  <Collapsible direction="horizontal" open={showSidebar}>
+                    <Box
+                      flex
+                      width="medium"
+                      background="light-2"
+                      elevation="small"
+                      align="center"
+                      justify="center"
+                    >
+                      SideBar for Settings.
+                    </Box>
+                  </Collapsible>
+                ) : (
+                  <Layer>
+                    <Box
+                      background="light-2"
+                      tag="header"
+                      justify="end"
+                      align="center"
+                      direction="row"
+                    >
+                      <Button
+                        icon={<FormClose />}
+                        onClick={() => setShowSidebar(false)}
+                      />
+                    </Box>
+                    <Box
+                      fill
+                      background="light-2"
+                      align="center"
+                      justify="center"
+                    >
+                      SideBar for Settings.
+                    </Box>
+                  </Layer>
+                )}
               </Box>
-            </Collapsible>
-          </Box>
-        </Box>
+            </Box>
+          )}
+        </ResponsiveContext.Consumer>
       </Grommet>
     </>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
